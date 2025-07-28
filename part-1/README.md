6ac4cf32e8b62944f1f269801e936554726d7bf9
Lordwin Suba <lordwin.suba@gmail.com>
2025-07-27 22:44:31 -0400

    prod deployment

---
+++ part-1/README.md
@@ -0,0 +1,1 @@
+cloud_storage_bucket_link: https://console.cloud.google.com/storage/browser/dl_bls__pub_time_series_pr_landing
--- part-4/scripts/deploy.sh
+++ part-4/scripts/deploy.sh
@@ -5,6 +5,7 @@ echo "${DIR}"
 CODE_PY_DIR="${DIR}/src"
 PROJECT_ID=$(gcloud config get-value project)
 SECRET_PROJECT=$(gcloud projects list --filter="$(gcloud config get-value project)" --format="value(PROJECT_NUMBER)")
+TRIGGER_CODE_PY_DIR="${DIR}/src_2"
 
 
 echo $CODE_PY_DIR
@@ -33,18 +34,36 @@ echo '#--- Update Cloud Run Revision ---#'
 gcloud run services update ${FUNCTION_NAME} --region=${REGION} --execution-environment=gen2
 
 
-echo '#--- Deployment of Cloud Scheduler ---#'
-gcloud scheduler jobs create http ${CLOUD_SCHEDULER_NAME} --location=${REGION} \
-    --schedule="${CS_SCHEDULER_RUN}" \
-    --uri="https://${REGION}-${PROJECT_ID}.cloudfunctions.net/${FUNCTION_NAME}" \
-    --http-method=${CS_HTTP_METHOD} \
-    --description="${CS_DESC}" \
-    --time-zone=${CS_TZ} \
-    --headers=Content-Type="application/json",User-Agent="Google-Cloud-Scheduler" \
-    --attempt-deadline=${CS_ATTEMPT_DEADLINE} \
-    --oidc-service-account-email=${SERVICE_ACCOUNT} \
-    --message-body='{"de_job_id":"'${DE_JOB_ID}'", "de_job_name":"'${DE_JOB_NAME}'", "project_name":"'${PROJECT_ID}'"}'
+# echo '#--- Deployment of Cloud Scheduler ---#'
+# gcloud scheduler jobs create http ${CLOUD_SCHEDULER_NAME} --location=${REGION} \
+#     --schedule="${CS_SCHEDULER_RUN}" \
+#     --uri="https://${REGION}-${PROJECT_ID}.cloudfunctions.net/${FUNCTION_NAME}" \
+#     --http-method=${CS_HTTP_METHOD} \
+#     --description="${CS_DESC}" \
+#     --time-zone=${CS_TZ} \
+#     --headers=Content-Type="application/json",User-Agent="Google-Cloud-Scheduler" \
+#     --attempt-deadline=${CS_ATTEMPT_DEADLINE} \
+#     --oidc-service-account-email=${SERVICE_ACCOUNT} \
+#     --message-body='{"de_job_id":"'${DE_JOB_ID}'", "de_job_name":"'${DE_JOB_NAME}'", "project_name":"'${PROJECT_ID}'"}'
 
+
+echo '#--- Deployment of Trigger CRF via GCS Event ---#'
+gcloud functions deploy ${FUNCTION_NAME} \
+    --region=${REGION} \
+    --source=${TRIGGER_CODE_PY_DIR} \
+    --entry-point=${ENTRY_POINT} \
+    --gen2 \
+    --trigger-bucket \
+    --trigger-resource=${BUCKET_NAME} \
+    --runtime=python312 \
+    --min-instances=${MIN_INSTANCES} \
+    --max-instances=${MAX_INSTANCES} \
+    --memory=${FUNCTION_MEMORY} \
+    --timeout=${TRIGGER_FUNCTION_TIMEOUT} \
+    --cpu=${CPU_LIMIT} \
+    --ingress-settings=all \
+    --no-allow-unauthenticated \
+    --update-labels=developer=lordwin,gcp-service=cloud_function
 
 
 echo '#--- Trigger Cloud Run Function ---#'
